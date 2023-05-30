7a928eec31b4402546daeb3f043375125a6130ea
SULTANUM MOBIN <119931140+smsultan76@users.noreply.github.com>
2023-05-30 19:53:23 +0600

    Update 002. Write a program to input and display all primitive data types of java..java
    2. Write a program to input and display all primitive data types of java.

--- Java/002. Write a program to input and display all primitive data types of java..java
+++ Java/002. Write a program to input and display all primitive data types of java..java
@@ -3,14 +3,13 @@ public class DefaultPrimitiveInOut {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Input data in this order:\n byte,\t char,\t short,\t int,\t long,\t float,\t double,\t Boolean.");
-         byte b = sc.nextByte();  //range -127 to 127
-         char c = sc.next().charAt(0);
-         short sh = sc.nextShort();
-         int i = sc.nextInt();
-         long L = sc.nextLong();
-         float f = sc.nextFloat();
-         double d = sc.nextDouble();
-         boolean bool = sc.nextBoolean();
-         System.out.println("Byte: "+b+"\nChar:"+c+"\nShort: "+sh+"\nInt: "+i+"\nLong: "+L+"\nFloat: "+f+"\nDouble: "+d+"\nBoolean: "+bool);    
-    }
-}
+         byte b = sc.nextByte();			 //range -128 to 127
+         char c = sc.next().charAt(0);		//range 0-65,535
+         short sh = sc.nextShort();			//range -32,768 to 32,767
+         int i = sc.nextInt();    			//range -2^31 to 2^31-1
+         long L = sc.nextLong(); 		            //range -2^63 to 2^63-1
+         float f = sc.nextFloat();			// 4 Byte
+         double d = sc.nextDouble();    		// 8 Byte
+         boolean bool = sc.nextBoolean();		// false(0) or true(1)
+         System.out.println("Byte: "+b+"\nChar:"+c+"\nShort: "+sh+"\nInt: "+i+"\nLong: "+L+"\nFloat: "+f+"\nDouble: "+d+"\nBoolean: "+bool);
+    }}
