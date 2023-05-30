fec4c22d3f7119073e4ff4cea5322008d9394c80
SULTANUM MOBIN <119931140+smsultan76@users.noreply.github.com>
2023-05-30 19:55:05 +0600

    Update 001. Write a program to display default value of all primitive data types of java.java
    1. Write a program to display default value of all primitive data types of java.

--- Java/001. Write a program to display default value of all primitive data types of java.java
+++ Java/001. Write a program to display default value of all primitive data types of java.java
@@ -4,13 +4,12 @@ class DataTypes{
 public class DefaultValue {
     public static void main(String[] args) {
         DataTypes dt=new DataTypes();
-        System.out.println("Dafault value of byte: "+dt.b);
-        System.out.println("Dafault value of char: "+dt.c);
-        System.out.println("Dafault value of Short: "+dt.sh);
-        System.out.println("Dafault value of Int: "+dt.i);
-        System.out.println("Dafault value of Long: "+dt.l);
-        System.out.println("Dafault value of Float: "+dt.f);
-        System.out.println("Dafault value of Double: "+dt.d);
-        System.out.println("Dafault value of Boolean: "+dt.bool);
-    }
-}
+        System.out.println("Dafault value of byte: "+dt.b);       	  // 0
+        System.out.println("Dafault value of char: "+dt.c);    	     //
+        System.out.println("Dafault value of Short: "+dt.sh);    	   // 0
+        System.out.println("Dafault value of Int: "+dt.i);         	 // 0
+        System.out.println("Dafault value of Long: "+dt.l);        	 // 0
+        System.out.println("Dafault value of Float: "+dt.f);      	  // 0.0
+        System.out.println("Dafault value of Double: "+dt.d);       	// 0.0
+        System.out.println("Dafault value of Boolean: "+dt.bool);   // false
+    }}
